9339fdb2e6008e7004ca46e75c6f9d2acacafe41
khyu2 <ikeelee7742@gmail.com>
2025-01-20 11:16:05 +0900

    feat: initial commit

---
+++ src/main/java/mars_6th/VER6/Ver6Application.java
@@ -0,0 +1,13 @@
+package mars_6th.VER6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ver6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ver6Application.class, args);
+	}
+
+}
---
+++ src/test/java/mars_6th/VER6/Ver6ApplicationTests.java
@@ -0,0 +1,13 @@
+package mars_6th.VER6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ver6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
