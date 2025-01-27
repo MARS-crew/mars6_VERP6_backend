dc1900caf2366e44a946ce32db6e78aaa75cb86d
khyu2 <ikeelee7742@gmail.com>
2025-01-27 11:40:34 +0900

    feat: swagger 명세 추가

--- src/main/java/mars_6th/VER6/domain/member/controller/MemberController.java
+++ src/main/java/mars_6th/VER6/domain/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package mars_6th.VER6.domain.member.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import mars_6th.VER6.domain.member.controller.dto.request.MemberRequest;
@@ -10,6 +12,7 @@ import mars_6th.VER6.domain.member.service.MemberService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "회원", description = "회원 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/loginAction")
@@ -17,6 +20,7 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "로그인")
     @PostMapping
     public ResponseEntity<?> login(@RequestBody MemberRequest req, HttpSession session) {
         return ResponseEntity.ok(memberService.login(req, session));
